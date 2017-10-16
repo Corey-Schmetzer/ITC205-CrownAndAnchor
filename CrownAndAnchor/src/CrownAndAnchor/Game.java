ea0e0ba60c78df6062475c0a82f3b566ffc2504d
Corey-Schmetzer <corey.schmetzer@gmail.com>
2017-10-16 22:11:02 +1100

    Fixed Bug 1
    Added bet onto winnings

--- CrownAndAnchor/src/CrownAndAnchor/Game.java
+++ CrownAndAnchor/src/CrownAndAnchor/Game.java
@@ -44,7 +44,7 @@ public class Game {
 		int winnings = matches * bet;
 
 		if (matches > 0) {			
-			player.receiveWinnings(winnings);
+			player.receiveWinnings(winnings + bet);
 		}
         return winnings;		
 	}
