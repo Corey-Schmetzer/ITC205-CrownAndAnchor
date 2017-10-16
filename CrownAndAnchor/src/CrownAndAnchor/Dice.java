1f1b00c6c8b8579b2cb6a112f8ca3318b6142ea3
Corey-Schmetzer <corey.schmetzer@gmail.com>
2017-10-16 19:13:16 +1100

    Moved src folder for easy access

---
+++ CrownAndAnchor/src/CrownAndAnchor/Dice.java
@@ -0,0 +1,22 @@
+package CrownAndAnchor;
+
+public class Dice {
+			
+	private DiceValue value;
+	
+	public Dice() {
+		value =  DiceValue.getRandom();
+	}
+	
+	public DiceValue getValue() {
+		return value;
+	}
+
+	public DiceValue roll() {
+		return DiceValue.getRandom();
+	}		
+	
+	public String toString() {
+		return value.toString();
+	}
+}
